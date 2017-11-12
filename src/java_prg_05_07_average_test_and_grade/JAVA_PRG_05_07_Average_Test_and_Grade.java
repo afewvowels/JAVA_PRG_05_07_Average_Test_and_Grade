a17e281a703d45250582ed9b9205a26a52706a0b
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 15:13:07 -0500

    Prompt written.

---
+++ src/java_prg_05_07_average_test_and_grade/JAVA_PRG_05_07_Average_Test_and_Grade.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_05_07_average_test_and_grade;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program that asks the user to enter five test scores. The program
+ * should display a letter grade for each score and the average test score.
+ * Write the following methods in the program:
+ * 
+ *      * calcAverage - This method should accept five test scores as arguments
+ *      and return the average of the scores.
+ *      * Determine grade - This method should accept a test score as an
+ *      argument and return a letter grade for the score, based on the following
+ *      grading scale:
+ * 
+ *          Score           Letter Grade
+ *          ----------------------------
+ *          90-100              A
+ *          80-89               B
+ *          70-79               C
+ *          60-69               D
+ *          Below 60            F
+ */
+public class JAVA_PRG_05_07_Average_Test_and_Grade {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
